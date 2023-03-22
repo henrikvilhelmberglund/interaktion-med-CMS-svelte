7fd33e56f8ac9f9b397f4a546ef40702fa0564a1
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-03-22 17:41:14 +0100

    feat: :sparkles: add dynamic route for blog posts

---
+++ src/routes/prismic/[uid]/+page.server.js
@@ -0,0 +1,19 @@
+import createClient from "$lib/prismicio";
+import * as prismicH from "@prismicio/helpers";
+
+const client = createClient();
+// const prismicQuery = client.getByUID("homepage", "homepage");
+// const prismicQuery = client.getSingle("homepage");
+
+export async function load({ fetch, request, params }) {
+	// const myUID = params; // Update for the UID of your homepage
+	console.log(params);
+	const client = createClient({ fetch, request });
+	const document = await client.getByUID("blog_post", params.uid);
+
+	if (document) {
+		return { document };
+	}
+
+	// error(404, "Not found");
+}
---
+++ src/routes/prismic/[uid]/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+	export let data;
+  import * as prismicH from "@prismicio/helpers";
+</script>
+
+<main class="[&>*]:m-4">
+	<!-- {#await prismicBlogQuery}
+		<p>Loading...</p>
+	{:then blogpost}
+		<div class="rounded-lg bg-slate-300 p-2">
+			<h1 class="text-3xl">{prismicH.asText(blogpost.data.blog_post_title)}</h1>
+			<img class="w-96" src={prismicH.asImageSrc(blogpost.data.blog_post_image)} alt="" />
+			{@html prismicH.asHTML(blogpost.data.blog_post_text_content)}
+		</div>
+	{:catch error}
+		<pre>{error.message}</pre>
+	{/await} -->
+  <div class="rounded-lg bg-slate-300 p-2">
+    <h1 class="text-3xl">{prismicH.asText(data.document.data.blog_post_title)}</h1>
+    <img class="w-96" src={prismicH.asImageSrc(data.document.data.blog_post_image)} alt="" />
+    {@html prismicH.asHTML(data.document.data.blog_post_text_content)}
+  </div>
+
+	<!-- <pre>{JSON.stringify(data.document, null, 2)}</pre> -->
+</main>
