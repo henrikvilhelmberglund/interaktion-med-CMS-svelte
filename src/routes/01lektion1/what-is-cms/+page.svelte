36e55b68028769174ba6a68eccb63021e0b985d5
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-03-22 10:39:54 +0100

    feat: :sparkles: add CMS explanation

---
+++ src/routes/01lektion1/what-is-cms/+page.svelte
@@ -0,0 +1,120 @@
+<script>
+</script>
+
+<!-- 
+
+Ta reda på följande:
+
+Vad är ett CMS och vad används det för?
+Vilka för- och nackdelar finns det med att använda ett CMS? När bör man använda det, och när behöver man inte använda det?
+Vad innebär Headless CMS?
+Vilka är för- och nackdelarna med att använda ett Headless CMS?
+Vilka är för- och nackdelarna med att använda ett traditionellt CMS (ej headless)?
+Vilka olika CMS-system finns det? Ge exempel på minst fem stycken.
+
+Källor ni kan använda för att hjälpa er hitta dessa svar!
+
+
+ -->
+
+<main class="[&>*]:m-4">
+	<p class="text-xl italic">Vad är ett CMS och vad används det för?</p>
+	<p>
+		Det är mjukvara med ett webbinterface som hjälper användare att skapa, hantera och redigera
+		content på en webbsida utan krav på teknisk kunskap. Alltså ett lättare sätt att bygga hemsidor
+		utan att behöva kunna koda.
+	</p>
+	<p class="text-xl italic">Vilka för- och nackdelar finns det med att använda ett CMS?</p>
+	<p class="font-bold">Fördelar:</p>
+	<p>En fördel är användarvänlighet för man behöver inte kunna koda.</p>
+	<p>Det går också snabbt att komma igång.</p>
+	<p>
+		Om man vill att sidan ska se ut på ett visst sätt finns det även ofta plugins eller teman som
+		man enkelt kan använda.
+	</p>
+	<p class="font-bold">Nackdelar:</p>
+	<p>
+		Några nackdelar är att det inte är lika flexibelt, t.ex om man behöver en väldigt specifik
+		feature och det inte finns en plugin kan det bli svårt.
+	</p>
+	<p>
+		Säkerhet kan också vara en nackdel för man måste se till att CMSen är uppdaterad. Vissa CMSer
+		kan vara resursintensiva och ladda långsamt. (fick 28 score i mobile på https://www.obama.org/)
+	</p>
+
+	<p>En sista nackdel kan vara priset, speciellt om man behöver någon plugin som kostar.</p>
+
+	<p class="text-xl italic">När bör man använda det, och när behöver man inte använda det?</p>
+	<p>
+		CMS kan vara användbart om man enkelt vill ha t.ex en blogg och vill ha ett smidigt sätt att
+		lägga upp nya inlägg.
+	</p>
+	<p>
+		CMS är inte lika användbart om man har väldigt specifika krav, lite innehåll eller behöver
+		väldigt bra prestanda.
+	</p>
+
+	<p class="text-xl italic">Vad innebär Headless CMS?</p>
+	<p>
+		Headless CMS innebär att man har en separation mellan innehållshanteringsfunktionerna och
+		presentationslagret.
+	</p>
+	<p class="text-xl italic">Vilka är för- och nackdelarna med att använda ett Headless CMS?</p>
+	<p class="font-bold">Fördelar:</p>
+	<p>
+		En fördel med headless CMS är att man får mer flexibilitet eftersom man har delat upp
+		innehållshanteringen och presentationen så man kan använda vilka ramverk eller tekniker man
+		vill.
+	</p>
+	<p>
+		En annan är att det kan vara enklare att integrera andra tjänster eller services som t.ex
+		e-handel eftersom headless CMS ofta designas med RESTful API.
+	</p>
+	<p>
+		En sista fördel är att det kan vara mer skalbart eftersom man kan utnyttja moderna
+		frontend-teknologier för presentationslagret.
+	</p>
+
+	<p class="font-bold">Nackdelar:</p>
+	<p>
+		En nackdel är att det kan vara kosta mer än en traditionell eftersom man måste skapa en frontend
+		också, man får den inte på köpet.
+	</p>
+	<p>
+		Det kan också vara mer tekniskt komplext så man behöver kunskap inom frontend för att effektivt
+		använda den. Det kan också vara svårare att komma igång/lära sig.
+	</p>
+	<p class="text-xl italic">
+		Vilka är för- och nackdelarna med att använda ett traditionellt CMS (ej headless)?
+	</p>
+	<p class="font-bold">Fördelar:</p>
+	<p>Enklare att använda. Helhetslösning. Mycket inbyggt.</p>
+	<p class="font-bold">Nackdelar:</p>
+	<p>
+		Inte lika flexibelt. Inte lika skalbart. Kan ha säkerhetsproblem (ett vanlig mål för hackers).
+	</p>
+	<p class="text-xl italic">Vilka olika CMS-system finns det? Ge exempel på minst fem stycken.</p>
+	<ol class="list-decimal pl-4">
+		<li>
+			<a class="underline underline-2 underline-blue-700" href="https://wordpress.org/">WordPress</a>
+		</li>
+		<li>
+			<a class="underline underline-2 underline-blue-700" href="https://www.joomla.org/">Joomla</a>
+		</li>
+		<li>
+			<a class="underline underline-2 underline-blue-700" href="https://www.drupal.org/">Drupal</a>
+		</li>
+		<li>
+			<a class="underline underline-2 underline-blue-700" href="https://prismic.io/">Prismic</a>
+		</li>
+		<li>
+			<a class="underline underline-2 underline-blue-700" href="https://www.sanity.io/">Sanity</a>
+		</li>
+		<li>
+			<a class="underline underline-2 underline-blue-700" href="https://strapi.io/">Strapi!</a>
+		</li>
+	</ol>
+</main>
+
+<style>
+</style>
