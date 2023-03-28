6a2803b7cdd3d5cb5cf57209ab43bcac6e921e82
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-03-28 21:54:31 +0200

    feat: :sparkles: add functionality for filtering by menu type

--- src/routes/restaurant/+page.svelte
+++ src/routes/restaurant/+page.svelte
@@ -1,9 +1,12 @@
 <script>
+	import { base } from "$app/paths";
 	import { quintOut } from "svelte/easing";
 	import { flip } from "svelte/animate";
 
+	let API_BASE = "http://127.0.0.1:1337/api/dishes?populate=*";
+
 	let getData = async () => {
-		let res = await fetch("http://127.0.0.1:1337/api/dishes?populate=*");
+		let res = await fetch(API_BASE);
 		let data = await res.json();
 		console.log(data);
 		return data;
@@ -29,57 +32,76 @@
 	};
 
 	let cap = (string) => {
-		return `${string.slice(0, 1).toUpperCase()}${string.slice(1)}`;
+		return `${string.slice(0, 1).toUpperCase()}${string.slice(1)}s`;
 	};
 
 	let allTypes = ["starter", "main", "dessert", "beverage"];
+  let allTypeLinks = [...allTypes];
 </script>
 
 <svelte:head />
 
-<main class="flex flex-col bg-black [&>*]:m-4">
+<main class="flex flex-col bg-black [&>*]:m-4 min-h-[95.9vh]">
 	<h1 class="font-rubik text-center text-6xl text-white ">Restaurant Ankademin</h1>
 
-	{#await getData()}
-		<p>Loading...</p>
-	{:then results}
-		<div class="bg-hero-i-like-food-light/5 bg-dark-900 flex flex-1 p-2 [&>*]:m-px">
-			{#each allTypes as type, i}
-				<article class="space-y-2">
-					<h2 class="font-arima py-6 text-left text-3xl text-white">{cap(type)}</h2>
-					{#each filter(sorted(Object.values(results.data)), type) as { id, attributes: { name, price, categories } }, i (id)}
-						<div
-							class="bg-hero-diagonal-stripes-light/1 font-arima rounded-md bg-slate-900 p-2 text-white"
-							animate:flip={{ delay: i * 50, duration: 1000, easing: quintOut }}>
-							<p class="text-2xl">
-								{name}
-								<span class="text-base">
-									{price}:-
-								</span>
-							</p>
+	<nav class="flex gap-4">
+		{#each ["all menu", ...allTypeLinks] as link}
+			<button
+				on:click={() => {
+					if (link === "all menu") {
+						API_BASE = "http://127.0.0.1:1337/api/dishes?populate=*";
+						allTypes = ["starter", "main", "dessert", "beverage"];
+					} else {
+						API_BASE = `http://127.0.0.1:1337/api/dishes?populate=*&filters[categories][name][$eq]=${link}`;
+						allTypes = [link];
+					}
+				}}
+				class="text-blue-500">{cap(link)}</button>
+		{/each}
+	</nav>
 
-							<ul class="list-none">
-								{#each Object.values(categories.data) as { attributes: { name: categoryName } }}
-									{#if categoryName !== type}
-										<li
-											class:nuts={categoryName === "contains nuts"}
-											class:on-sale={categoryName === "on sale"}
-											class:vegetarian={categoryName === "vegetarian"}>
-											{categoryName}
-										</li>
-									{/if}
-								{/each}
-							</ul>
-						</div>
-					{/each}
-				</article>
-			{/each}
-		</div>
-	{:catch error}
-		<p>
-			{error}
-		</p>
-	{/await}
+	{#key API_BASE}
+		{#await getData()}
+			<p>Loading...</p>
+		{:then results}
+			<div class="bg-hero-i-like-food-light/5 bg-dark-900 flex flex-1 p-2 [&>*]:m-px">
+				{#each allTypes as type}
+					<article class="space-y-2">
+						<h2 class="font-arima py-6 text-left text-3xl text-white">{cap(type)}</h2>
+						{#each filter(sorted(Object.values(results.data)), type) as { id, attributes: { name, price, categories } }, i (id)}
+							<div
+								class="bg-hero-diagonal-stripes-light/1 font-arima rounded-md bg-slate-900 p-4 text-white"
+								animate:flip={{ delay: i * 50, duration: 1000, easing: quintOut }}>
+								<p class="text-2xl">
+									{name}
+									<span class="text-base">
+										{price}:-
+									</span>
+								</p>
+
+								<ul class="list-none">
+									{#each Object.values(categories.data) as { attributes: { name: categoryName } }}
+										{#if categoryName !== type}
+											<li
+												class:nuts={categoryName === "contains nuts"}
+												class:on-sale={categoryName === "on sale"}
+												class:vegetarian={categoryName === "vegetarian"}>
+												{categoryName}
+											</li>
+										{/if}
+									{/each}
+								</ul>
+							</div>
+						{/each}
+					</article>
+				{/each}
+			</div>
+		{:catch error}
+			<p>
+				{error}
+			</p>
+		{/await}
+	{/key}
 </main>
 
 <style>
