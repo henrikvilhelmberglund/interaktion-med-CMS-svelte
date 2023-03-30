63e96e68a2d1469738e3f335fb169ce69452d0eb
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-03-30 08:55:54 +0200

    feat: :sparkles: add 04lektion4 route

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
 	import Nav from "$lib/Nav.svelte";
 	import "@unocss/reset/antfu.css";
-	let routes = ["01lektion1", "prismic", "strapi", "03lektion3", "restaurant"];
+	let routes = ["01lektion1", "prismic", "strapi", "03lektion3", "restaurant", "04lektion4"];
 	let prefix = "/";
 </script>
 
---
+++ src/routes/04lektion4/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+  
+</script>
+
+<main class="[&>*]:m-4">
+<p class="text-xl">Auth</p>
+<p>Text</p>
+</main>
+
+<style>
+  
+</style>
